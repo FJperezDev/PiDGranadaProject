797a3345c03c1d4910f7a1ef2b34a453b3c25623
FJperezDev <fjperez.dev@gmail.com>
2025-12-13 23:10:32 +0100

    encryption bug solved

--- frontendTeacher/utils/encryption.js
+++ frontendTeacher/utils/encryption.js
@@ -2,8 +2,11 @@ import forge from 'node-forge';
 import { Buffer } from 'buffer';
 
 // Decodificamos de Base64 a string normal PEM
-let publicKey = process.env.EXPO_PUBLIC_KEY_B64
-const PUBLIC_KEY_PEM = null
+let publicKey = process.env.EXPO_PUBLIC_KEY_B64;
+
+// CAMBIO AQUÍ: 'let' en lugar de 'const' para poder modificarlo abajo
+let PUBLIC_KEY_PEM = null; 
+
 if (publicKey) 
   PUBLIC_KEY_PEM = Buffer.from(process.env.EXPO_PUBLIC_KEY_B64, 'base64').toString('utf-8');
 
@@ -18,7 +21,7 @@ export const encryptPassword = (password) => {
   try {
     const publicKey = forge.pki.publicKeyFromPem(PUBLIC_KEY_PEM);
     
-    // Encriptar usando RSA-OAEP con SHA-256 (mismo padding que pusimos en Django)
+    // Encriptar usando RSA-OAEP con SHA-256
     const encrypted = publicKey.encrypt(password, 'RSA-OAEP', {
       md: forge.md.sha256.create(),
       mgf1: {
@@ -26,7 +29,7 @@ export const encryptPassword = (password) => {
       }
     });
 
-    // Convertir a base64 para enviarlo por la red
+    // Convertir a base64
     return forge.util.encode64(encrypted);
   } catch (error) {
     console.error("Encryption error:", error);
