23f93d72c9de9f1a1a309992f64f66d48bf6f085
FJperezDev <fjperez.dev@gmail.com>
2025-12-13 23:23:09 +0100

    .

--- backend/apps/customauth/apps.py
+++ backend/apps/customauth/apps.py
@@ -9,21 +9,21 @@ class ApiConfig(AppConfig):
     
     def ready(self):
         # if settings.DEBUG:
-            try:
-                CustomTeacher = get_user_model()
-                if not CustomTeacher.objects.filter(is_superuser=True).exists():
-                    CustomTeacher.objects.create_superuser(
-                        username='admin',
-                        email='admin@admin.com',
-                        password='admin123',
-                        is_super=True
-                    )
-                    CustomTeacher.objects.create_user(
-                        username='fran',
-                        email='fran@gmail.com',
-                        password='fran123',
-                        is_super=False
-                    )
-            except (OperationalError, ProgrammingError):
-                # Base de datos aún no está lista (por ejemplo al correr 'migrate')
-                pass
+        try:
+            CustomTeacher = get_user_model()
+            if not CustomTeacher.objects.filter(is_superuser=True).exists():
+                CustomTeacher.objects.create_superuser(
+                    username='admin',
+                    email='admin@admin.com',
+                    password='admin123',
+                    is_super=True
+                )
+                CustomTeacher.objects.create_user(
+                    username='fran',
+                    email='fran@gmail.com',
+                    password='fran123',
+                    is_super=False
+                )
+        except (OperationalError, ProgrammingError):
+            # Base de datos aún no está lista (por ejemplo al correr 'migrate')
+            pass
