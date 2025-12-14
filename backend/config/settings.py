97b7aaed060caee90e4873d62dd77db3cf8f57c9
FJperezDev <fjperez.dev@gmail.com>
2025-12-14 11:44:20 +0100

    .

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,8 +84,8 @@ MIDDLEWARE = [
 ]
 
 SIMPLE_JWT = {
-    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+#    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
